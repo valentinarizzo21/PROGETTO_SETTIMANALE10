ed0b1c5a23ea7e4e0c599847ad37fa86f47a215a
valentinarizzo21 <rizzo.val3ntina@gmail.com>
2025-01-17 14:28:13 +0100

    implementation of react-router-dom

--- meteo-react/src/App.jsx
+++ meteo-react/src/App.jsx
@@ -2,13 +2,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import WeatherSec from "./components/WeatherSec";
 import NavBar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app">
-      <NavBar />
-      <WeatherSec />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<WeatherSec />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
